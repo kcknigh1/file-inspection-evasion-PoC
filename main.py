b027f9b8623682f1cedb7e67075f66c1f60b9fc4
Kyle Knight <kyle-knight@hotmail.com>
2021-06-05 21:41:25 -0500

    move the main function to the top level dir

---
+++ main.py
@@ -0,0 +1,42 @@
+"""The main file to run the program from the command line"""
+
+
+import argparse
+import exploit.file_inspection_evasion as fie
+
+
+def main(file_loc):
+    """The main function for the program
+
+    Args:
+        file_loc (str): The location of the original zip file for the exploit
+    """
+    original_file_hex = fie.read_in_file(file_loc)
+    local_files, central_directory, end_central_directory = fie._split_zip_file_into_sections(
+        original_file_hex)
+    # print('\n\n\n')
+    # print(original_file_hex)
+    # print('\n\n\n')
+    # print(local_files)
+    # print('\n\n\n')
+    # print(central_directory)
+    # print('\n\n\n')
+    # print(end_central_directory)
+    # print('\n\n\n')
+    fie.write_out_file(file_loc=file_loc,
+                       contents=f'{local_files}')
+
+
+#  TODO Look up argparser examples
+if __name__ == '__main__':
+    # TODO update the description
+    msg = "Test Description"
+
+    parser = argparse.ArgumentParser(description=msg)
+
+    parser.add_argument("-f", "--File", required=True, help="zip File to edit")
+
+    args = parser.parse_args()
+
+    if args.File:
+        main(file_loc=args.File)
