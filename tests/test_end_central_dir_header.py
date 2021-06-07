6e0a1f463e9b730e21fc3b02acf106d7cb58dea1
Kyle Knight <kyle-knight@hotmail.com>
2021-06-07 00:51:27 -0500

    clean up utils.py

--- tests/test_end_central_dir_header.py
+++ tests/test_end_central_dir_header.py
@@ -9,8 +9,6 @@ class TestEndCentralDirHeaderInit(unittest.TestCase):
         header_hex = '504b050600000000010001005b000000278e00000000'
         end_header = end_central_header.EndCentralDirectoryHeader(header_hex)
         self.assertEqual(end_header._header_hex, header_hex)
-        self.assertEqual(end_header._num_central_dir_records, 1)
+        self.assertEqual(end_header.get_num_central_dir_records(), 1)
         self.assertEqual(
-            end_header._offset_start_central_dir_from_start, 36391)
-        self.assertEqual(end_header._comment_length, 0)
-# class TestEndCentralDirHeaderGetNum(unittest.TestCase):
+            end_header.get_offset_start_central_dir_from_start(), 36391)
